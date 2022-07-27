7d9fe126a31b8c75139f645173c169e6a57c975a
Aditya Gupta <ashishgupta9936ad@gmail.com>
2022-07-28 00:49:43 +0530

    Initial Commit

---
+++ app/src/main/java/com/sparklead/shoppinglist/ui/shoppinglist/ShoppingActivity.kt
@@ -0,0 +1,15 @@
+package com.sparklead.shoppinglist.ui.shoppinglist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sparklead.shoppinglist.R
+
+class ShoppingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping)
+
+        
+
+    }
+}
